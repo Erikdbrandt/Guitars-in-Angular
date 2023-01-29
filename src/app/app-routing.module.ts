5769951323907a204aeba471f5c06d9dfac3c38a
Erik Brandt <erikdbrandt@gmail.com>
2023-01-29 11:15:07 +0100

    Second video Adding routes and setting up

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {LoginPage} from "./pages/login/login.page";
+import {ProfilePage} from "./pages/profile/profile.page";
+import {GuitarCataloguePage} from "./pages/guitar-catalogue/guitar-catalogue.page";
+
+
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginPage
+  },
+
+  {
+    path: "guitars",
+    component: GuitarCataloguePage
+  },
+
+  {
+    path: "profile",
+    component: ProfilePage
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], //import a module
+  exports: [RouterModule] // expose module and its features to other modules
+})
+
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AppRoutingModule} from "./app-routing.module";
+
 
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { GuitarCataloguePage } from './pages/guitar-catalogue/guitar-catalogue.page';
+import { ProfilePage } from './pages/profile/profile.page';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    GuitarCataloguePage,
+    ProfilePage
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
